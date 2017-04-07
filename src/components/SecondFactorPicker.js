cb09a5c3c3a6b11b60d2fc6a0afdf5ec6b3abc3b
Ryan Kurte <ryankurte@gmail.com>
2017-04-07 18:15:01 +1200

    Always HTTPS

--- src/components/SecondFactorPicker.js
+++ src/components/SecondFactorPicker.js
@@ -40,18 +40,18 @@ SecondFactorPicker.propTypes = {
 SecondFactorPicker.u2fInfo = {
   type: "Universal Second Factor (U2F/FIDO)",
   description: "Authenticate using a U2F (or FIDO) Token such as a Yubikey",
-  link: "/2fa/u2f",
+  link: "#/2fa-u2f-authorize",
 }
 SecondFactorPicker.totpInfo = {
   type: "Time based One Time Pad (TOTP)",
   description: "Authenticate using a TOTP application such as Google Authenticator",
-  link: "/2fa/totp",
+  link: "#/2fa-totp-authorize",
 }
 
 SecondFactorPicker.backupInfo = {
   type: "Backup Code",
   description: "Authenticate using an offline backup code",
-  link: "/2fa/backup",
+  link: "#/2fa-backup-authorize",
 }
 
 export {SecondFactorPicker, SecondFactorLink}
